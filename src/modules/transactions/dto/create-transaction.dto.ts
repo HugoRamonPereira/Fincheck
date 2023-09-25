3f8b0827e1a0d22287f09221da2a062bdf483191
Hugo Ramon <ramone.techie@gmail.com>
2023-09-25 11:36:20 -0300

    created validation for the transactinons

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,39 @@
-export class CreateTransactionDto {}
+import {
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+import { TransactionType } from '../entities/Transaction';
+
+export class CreateTransactionDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  bankAccountId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  categoryId: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  value: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  date: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEnum(TransactionType)
+  type: TransactionType;
+}
