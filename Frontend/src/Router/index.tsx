074cf7c0d4ef31556ed004619c7d0e9e36555197
Hugo Ramon <ramone.techie@gmail.com>
2023-09-26 14:01:51 -0300

    Created layouot, signin, signup, button and input components

--- Frontend/src/Router/index.tsx
+++ Frontend/src/Router/index.tsx
@@ -1,17 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthGuard } from './AuthGuard';
+import { Signin } from '../view/pages/Signin';
+import { Signup } from '../view/pages/Signup';
+import { Dashboard } from '../view/pages/Dashboard';
+import { AuthLayout } from '../view/layouts/AuthLayout';
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AuthGuard isPrivate={false} />}>
-          <Route path='/login' element={<h1>Login</h1>} />
-          <Route path='/register' element={<h1>Register</h1>} />
+          <Route element={<AuthLayout />}>
+            <Route path='/signin' element={< Signin/>} />
+            <Route path='/signup' element={<Signup />} />
+          </Route>
         </Route>
 
         <Route element={<AuthGuard isPrivate />}>
-          <Route path='/' element={<h1>Dashboard</h1>} />
+          <Route path='/' element={<Dashboard />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ Frontend/src/view/pages/Signin/index.tsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+export function Signin() {
+  return (
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-semibold text-gray-900">
+        Log into your account
+        </h1>
+
+        <p className='space-x-3'>
+          <span className='text-gray-700'>
+          Don&apos;t have an account?
+          </span>
+          <Link to='/signup' className='text-teal-900 font-medium'>
+            Sign up
+          </Link>
+        </p>
+      </header>
+
+      <form className='mt-16 flex flex-col gap-4'>
+        <Input
+          type="email"
+          placeholder='Email'
+          name='email'
+        />
+        <Input
+          type="password"
+          placeholder='Password'
+          name='password'
+        />
+        <Button type="submit" className='mt-2'>Sign in</Button>
+      </form>
+    </>
+  );
+}
