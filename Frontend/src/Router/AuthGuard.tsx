98dbfe1dd8d6215997cb2737e1ac6f098d9afe37
Hugo Ramon <ramone.techie@gmail.com>
2023-09-25 23:34:49 -0300

    Added Routing, AuthGuard, private and public routes and scaffolded the project folder structure

---
+++ Frontend/src/Router/AuthGuard.tsx
@@ -0,0 +1,22 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+type AuthGuardProps = {
+  isPrivate: boolean;
+}
+
+export function AuthGuard({ isPrivate }: AuthGuardProps) {
+  const signedIn = false;
+
+  // The property replace was added to avoid saving browsing history in cache
+  // So if the user is trying to access a private route the route will be replaced and there will be no history
+  // No history for the user to be going back to, by clicking in the arrows
+  if (!signedIn && isPrivate) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (signedIn && !isPrivate) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
+}
