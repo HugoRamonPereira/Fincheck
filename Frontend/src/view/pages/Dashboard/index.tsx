9cd5318ac760416548fac9255517d4c9bc5c9526
Hugo Ramon <ramone.techie@gmail.com>
2023-09-29 23:26:42 -0300

    Created signin and signout functions and store/remove the accessToken in/from localStorage

--- Frontend/src/view/pages/Dashboard/index.tsx
+++ Frontend/src/view/pages/Dashboard/index.tsx
@@ -1,5 +1,17 @@
+import { useAuth } from '../../../app/hooks/useAuth/useAuth';
+import { Button } from '../../components/Button';
+
 export function Dashboard() {
+  // The function destructured signout comes from the useAuth context
+  const { signout } = useAuth();
+
   return (
-    <h1>Dashboard Page</h1>
+    <>
+      <h1>Dashboard Page</h1>
+
+      <Button onClick={signout}>
+        Sign out
+      </Button>
+    </>
   );
 }
