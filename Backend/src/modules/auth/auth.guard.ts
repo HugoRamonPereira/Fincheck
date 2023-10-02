d97f0734e5ac9eaddef9dcb5d487de7ca6d1af84
Hugo Ramon <ramone.techie@gmail.com>
2023-10-01 23:40:04 -0300

    Added message in Authguard for the errors with token or invalid credentials

--- Backend/src/modules/auth/auth.guard.ts
+++ Backend/src/modules/auth/auth.guard.ts
@@ -35,7 +35,7 @@ export class AuthGuard implements CanActivate {
     const token = this.extractTokenFromHeader(request);
 
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Access token not found!');
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
@@ -44,7 +44,7 @@ export class AuthGuard implements CanActivate {
 
       request['userId'] = payload.sub;
     } catch {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Invalid credentials!');
     }
     return true;
   }
