fbf0f2b9d7afb2e3fd0cb7c5b7591ffe0a40b81b
Hugo Ramon <ramone.techie@gmail.com>
2023-10-19 19:51:32 -0300

    Added calendar/datepicker and styled it

---
+++ Frontend/src/view/components/DatePicker/index.tsx
@@ -0,0 +1,39 @@
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
+import { enUS } from 'date-fns/locale';
+import { capitalizeFirstLetter } from '../../../app/utils/capitalizeFirstLetter';
+
+interface DatePickerProps {
+  value: Date;
+  onChange(date: Date): void;
+}
+
+export function DatePicker({ value, onChange }: DatePickerProps) {
+  return (
+    <DayPicker
+      locale={enUS}
+      selected={value}
+      mode='single'
+      onSelect={(date) => onChange(date ?? new Date())}
+      classNames={{
+        caption: 'flex items-center justify-between',
+        nav: 'flex gap-1',
+        nav_button_previous: 'text-teal-800 flex items-center justify-center !bg-transparent',
+        nav_button_next: 'text-teal-800 flex items-center justify-center !bg-transparent',
+        head_cell: 'uppercase text-xs text-gray-500 font-Montserrat font-medium pt-1 pb-2',
+        button: 'text-gray-700 cursor-pointer font-Montserrat w-10 h-10 hover:bg-teal-100 rounded-full',
+        day_today: 'bg-gray-100 font-Montserrat font-bold text-gray-900',
+        day_selected: '!bg-teal-900 text-white font-Montserrat font-medium'
+      }}
+      formatters={{
+        formatCaption: (date, options) => {
+          return (
+            <span className='font-Montserrat text-gray-900 font-medium ml-2'>
+              {capitalizeFirstLetter(format(date, 'LLLL yyy', options))}
+            </span>
+          );
+        }
+      }}
+    />
+  );
+}
--- Frontend/src/view/components/DatePickerInput/index.tsx
+++ Frontend/src/view/components/DatePickerInput/index.tsx
@@ -2,6 +2,8 @@ import { CrossCircledIcon } from '@radix-ui/react-icons';
 import { cn } from '../../../app/utils/cn';
 import { useState } from 'react';
 import { formatDate } from '../../../app/utils/formatDate';
+import { Popover } from '../Popover';
+import { DatePicker } from '../DatePicker';
 
 interface DatePickerProps {
   error?: string;
@@ -13,20 +15,31 @@ export function DatePickerInput({ error, className }: DatePickerProps) {
 
   return (
     <div>
-      <button
-        type='button'
-        className={cn('bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] font-Montserrat text-gray-800 focus:border-gray-800        transition-all outline-none text-left relative pt-4',
-          error && '!border-red-900',
-          className
-        )}
-      >
-        <span className='absolute text-gray-700 text-[10px] left-[13px] top-2 font-Montserrat pointer-events-none'>
-          Date
-        </span>
-        <span>
-          {formatDate(selectedDate)}
-        </span>
-      </button>
+      <Popover.Root>
+        <Popover.Trigger>
+          <button
+            type='button'
+            className={cn('bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] font-Montserrat text-gray-800 focus:border-gray-800        transition-all outline-none text-left relative pt-4',
+              error && '!border-red-900',
+              className
+            )}
+          >
+            <span className='absolute text-gray-700 text-[10px] left-[13px] top-2 font-Montserrat pointer-events-none'>
+              Date
+            </span>
+            <span>
+              {formatDate(selectedDate)}
+            </span>
+          </button>
+        </Popover.Trigger>
+
+        <Popover.Content>
+          <DatePicker
+            value={selectedDate}
+            onChange={date => setSelectedDate(date)}
+          />
+        </Popover.Content>
+      </Popover.Root>
 
       {error && (
         <div className='flex items-center gap-1 mt-2 text-red-900'>
